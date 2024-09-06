6af2db1ae8619ad011ec5f968a04b0b1262d6cc6
berlinlee-phoenix <berlinlee2017@gmail.com>
2024-09-06 17:24:03 +0800

    fixing img

--- tree/readme.md
+++ tree/readme.md
@@ -6,8 +6,8 @@
 <h2>2. Creating 2nd arrow function using .findIndex() method</h2>
 <img style="align: center; aspect-ratio: 16/9; max-width: 500px;" src="./images/findIndex.jpg">
 <br>
-<h2>3. Creating an array => Start console.time() & console.timeEnd() to measure to differences</h2>
-<img style="align: center; aspect-ratio: 16/9; max-width: 500px;" src="./images/consoleTime.jpg">
+<h2>3. Creating an array => Invoke treeSearch vs findIndex methods</h2>
+<img style="align: center; aspect-ratio: 16/9; max-width: 500px;" src="./images/consoleLog.jpg">
 <br>
 <h2>4. Using Code Running vscode extension to reflect results</h2>
 <img style="align: center; aspect-ratio: 16/9; max-width: 500px;" src="./images/result.jpg">
--- tree/tree-search.js
+++ tree/tree-search.js
@@ -1,6 +1,7 @@
 strict = 'true';
 
 const treeSearch = (sortedArray, target) => {
+    let t0 = performance.now();
     let left = 0;
     let right = sortedArray.length - 1;
 
@@ -9,55 +10,54 @@ const treeSearch = (sortedArray, target) => {
         const midValue = sortedArray[mid];
 
         if (midValue === target) {
+            let t1 = performance.now();
+            console.log(`Call to find Index took ${(t1 - t0)}ms\n`);
             return mid; // Target found, return its index
         } else if (midValue < target) {
+            let t1 = performance.now();
+            console.log(`Call to find Index took ${(t1 - t0)}ms\n`);
             left = mid + 1; // Focus on the right half
         } else {
+            let t1 = performance.now();
+            console.log(`Call to find Index took ${(t1 - t0)}ms\n`);
             right = mid - 1; // Focus on the left half
         }
     }
-
+    let t1 = performance.now();
+    console.log(`Call to find Index took ${(t1 - t0)}ms\n`);
     return -1; // Target not found in the array
 }
 
 // Using native JavaScript .find() method
 const normalSearch = (sortedArray, target) => {
+    let t2 = performance.now();
     const targetIndex = sortedArray.findIndex(element => element === target);
+
+    let t3 = performance.now();
+    console.log(`Call to find Index took ${(t3 - t2)}ms\n`);
     return targetIndex;
 }
 
 const sortedArray = [1, 5, 8, 12, 15, 19, 23, 31, 7, 56];
 
-console.time();
 const index15 = normalSearch(sortedArray, 15);
-console.log(`normalSearch(sortedArray, 15):\n${index15}`); // indexAt: 4
-console.timeEnd(); // default: 1.117ms on a mac mini Apple M2 16GB Memory Ventura 13.2.1
-console.log(`\n`);
-console.time();
+console.log(`normalSearch(sortedArray, 15):\n${index15}\n`); 
+
 const input15 = treeSearch(sortedArray, 15);
-console.log(`treeSearch(sortedArray, 15):\n${input15}`); // indexAt: 4
-console.timeEnd(); // default: 0.035ms on a mac mini Apple M2 16GB Memory Ventura 13.2.1
+console.log(`treeSearch(sortedArray, 15):\n${input15}\n`); // indexAt: 4
 
 console.log(`\n\n`);
 
-console.time();
 const index19 = normalSearch(sortedArray, 19);
-console.log(`normalSearch(sortedArray, 19):\n${index19}`); // indexAt: 5
-console.timeEnd(); // default: 0.01ms on a mac mini Apple M2 16GB Memory Ventura 13.2.1
+console.log(`normalSearch(sortedArray, 19):\n${index19}\n`); // indexAt: 5
 console.log(`\n`);
-console.time();
 const input19 = treeSearch(sortedArray, 19);
-console.log(`treeSearch(sortedArray, 19):\n${input19}`); // indexAt: 5
-console.timeEnd(); // default: 0.224ms on a mac mini Apple M2 16GB Memory Ventura 13.2.1
+console.log(`treeSearch(sortedArray, 19):\n${input19}\n`); // indexAt: 5
 
 console.log(`\n\n`);
 
-console.time();
 const index31 = normalSearch(sortedArray, 31);
 console.log(`normalSearch(sortedArray, 31):\n${index31}`); // indexAt: 7
-console.timeEnd(); // default: 0.018ms on a mac mini Apple M2 16GB Memory Ventura 13.2.1
 console.log(`\n`);
-console.time();
 const input31 = treeSearch(sortedArray, 31);
 console.log(`treeSearch(sortedArray, 31):\n${input31}`); // indexAt: 7
-console.timeEnd(); // default: 0.016ms on a mac mini Apple M2 16GB Memory Ventura 13.2.1
