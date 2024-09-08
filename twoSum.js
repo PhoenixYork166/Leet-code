693729518c323f355a6bae0a0d1c4b71dd31f039
berlinlee-phoenix <berlinlee2017@gmail.com>
2024-09-08 21:35:42 +0800

    commit

--- twoSum.js
+++ twoSum.js
@@ -36,37 +36,35 @@
 // complement_pair = 10 - 9 = 1
 // [0, 2]
 
-// const nums = [3,2,4]
-// target = 6
-
-// var twoSum = function(nums, target) {
-//     var map = {};
-//     for (let i=0; i < nums.length; i++) {
-//         var value = nums[i];
-//         var complementPair = target - value;
-
-//         if (map[complementPair] !== undefined) {
-//             return [map[complementPair], i];
-//         } else {
-//             map[value] = i;
-//         }
-//     }
-// }
-
-
 const nums = [3,2,4,1,2,3,3]
-const target = 6
+target = 6
 
-const twoSum = (nums, target) => {
-    for (let i=0; i < nums.length; i++) {
-        for (let j=i+1; j < nums.length; j++) {
-            if (nums[i] + nums[j] === target) {
-                console.log(`sum to 6: array[${[i]}] + array[${[j]}]`);
-                // return [i, j];
-            }
+var twoSum = function(nums, target) {
+    var map = {}; // O(1) - Initializing a map is a constant time operations
+
+    for (let i=0; i < nums.length; i++) { // O(n) - The loop runs n times where n = nums.length
+        var value = nums[i]; // O(1) - Accessing an array element by index is a constant time operation
+        var complementPair = target - value; // O(1) - Calculating the complement is a constant time arithmetic operation
+
+        if (map[complementPair] !== undefined) { // O(1) - Checking for a property in a hashMap is a constant time operation
+            return [map[complementPair], i]; // O(1) - Return array elements is a constant time operation
+        } else {
+            map[value] = i; // O(1) - Setting a property in a hashMap is a constant time operation
         }
     }
-    
-};
+}
+console.log(twoSum(nums, target)); // O(1) - console.log is outside the loop & runs in constant time
+// O(n + 7) => O(7n)
 
-twoSum(nums, target);
+/* O(n^2) performance */
+// const twoSum = (nums, target) => {
+//     for (let i=0; i < nums.length; i++) {
+//         for (let j=i+1; j < nums.length; j++) {
+//             if (nums[i] + nums[j] === target) {
+//                 console.log(`sum to 6: array[${[i]}] + array[${[j]}]`);
+//                 // return [i, j];
+//             }
+//         }
+//     }    
+// };
+// twoSum(nums, target);
